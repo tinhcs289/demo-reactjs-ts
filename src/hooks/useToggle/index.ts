1ee616c9ecd829c10611497bb223f6c3d935f0e8
tinh.cs [CMC] <tinhcs289@outlook.com>
2022-11-11 17:25:39 +0700

    add Dashboard layout

--- src/hooks/useToggle/index.ts
+++ src/hooks/useToggle/index.ts
@@ -2,7 +2,20 @@ import { useCallback, useState } from 'react';
 
 const useToggle = (initialState: boolean = false): [boolean, () => void] => {
   const [state, setState] = useState<boolean>(initialState);
-  const toggle = useCallback((): void => setState((state) => !state), []);
+  const toggle = useCallback((value?: boolean): void => {
+    if (value === true) {
+      setState(true);
+      return;
+    }
+
+    if (value === false) {
+      setState(false);
+      return;
+    }
+
+    setState((state) => !state);
+    return;
+  }, []);
   return [state, toggle];
 };
 export default useToggle;
